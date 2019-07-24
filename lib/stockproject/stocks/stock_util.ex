13d367cf5f87a621947530a58c7b52e1d823aa14
peng <peng.zi@husky.neu.edu>
2019-07-24 05:51:16 +0000

    iex trading api update

--- lib/stockproject/stocks/stock_util.ex
+++ lib/stockproject/stocks/stock_util.ex
@@ -2,8 +2,10 @@ defmodule StockUtil do
 
 #use close prices in one year to calculate stock's risk
   def calc_risk(abbreviation) do
-    url = "https://api.iextrading.com/1.0/stock/#{abbreviation}/chart/1y"
+    token=Application.fetch_env!(:stockproject, :iex_token)
+    url = "https://cloud.iexapis.com/stable/stock/#{abbreviation}/chart/1y?token=#{token}"
     resp = HTTPoison.get!(url)
+    IO.inspect(resp.body)
     data = Jason.decode!(resp.body)
     close = Enum.map(data, fn x -> x["close"] end)
     risk = Float.round(Statistics.stdev(close)/Statistics.mean(close) * 100, 2)
@@ -12,7 +14,8 @@ defmodule StockUtil do
 
   #get the annual rate of return
   def get_annual_ror(abbreviation) do
-    url = "https://api.iextrading.com/1.0/stock/#{abbreviation}/chart/2y"
+    token=Application.fetch_env!(:stockproject, :iex_token)
+    url = "https://cloud.iexapis.com/stable/stock/#{abbreviation}/chart/2y?token=#{token}"
     resp = HTTPoison.get!(url)
     data = Jason.decode!(resp.body)
     ror=List.last(data)["changeOverTime"]
@@ -27,20 +30,24 @@ defmodule StockUtil do
   end
 
   def get_logo(abbrev) do
-    resp = HTTPoison.get!("https://api.iextrading.com/1.0/stock/#{abbrev}/logo")
+    token=Application.fetch_env!(:stockproject, :iex_token)
+    resp = HTTPoison.get!("https://cloud.iexapis.com/stable/stock/#{abbrev}/logo?token=#{token}")
     data = Jason.decode!(resp.body)
     data["url"]
   end
 
   def get_current_price(abbrev) do
-    url = "https://api.iextrading.com/1.0/stock/#{abbrev}/price"
+    token=Application.fetch_env!(:stockproject, :iex_token)
+    url = "https://cloud.iexapis.com/stable/stock/#{abbrev}/price?token=#{token}"
     resp = HTTPoison.get!(url)
     Jason.decode!(resp.body)
   end
 
   def get_history(abbrev, time_span) do
-    url = "https://api.iextrading.com/1.0/stock/#{abbrev}/chart/#{time_span}"
+    token=Application.fetch_env!(:stockproject, :iex_token)
+    url = "https://cloud.iexapis.com/stable/stock/#{abbrev}/chart/#{time_span}?token=#{token}"
     resp = HTTPoison.get!(url)
+    IO.inspect(resp)
     data = Jason.decode!(resp.body)
     Enum.map(data, fn x -> %{close: x["close"], date: x["date"]} end)
   end
